8d3eb936071b14f13067edb4d727080bbe1bf0f8
Demchenko Kirill <thegalaxydias@gmail.com>
2021-10-18 19:49:48 +0300

    Добавьте файлы проекта.

---
+++ Selenium.Screenshot/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.Screenshot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            driver.Manage().Window.Maximize();
+
+            ITakesScreenshot screen = (ITakesScreenshot)driver;
+            screen.GetScreenshot().SaveAsFile(@"G:\google.png");
+
+            //IWebElement upload = driver.FindElement(By.Id("google"));
+            //upload.SendKeys(@"C:\");
+
+        }
+    }
+}
